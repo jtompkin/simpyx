3b12ce5abb192c129dc46e0684362de84dc6af61
jtompkin <jtompkin-dev@pm.me>
2024-08-27 22:51:52 -0400

    BIG

--- main.py
+++ main.py
@@ -1,27 +1,52 @@
 #!/usr/bin/env python3
-import time
+import sys
+from time import sleep
 
 import pixels
 
 
-DELAY = 0.05
-N = 60
+def static(pix: pixels.Pixels) -> None:
+    pix[3] = pixels.Color(100, 20, 255)
+    pix.show()
+    input()
+
+
+def cycle(pix: pixels.Pixels) -> None:
+    delta = 255 / len(pix)
+    while True:
+        pix.fill(pixels.Color(0, 0, 0))
+        for i in range(len(pix)):
+            pix[i] = pixels.Color(int((i + 1) * delta), 0, 100)
+            pix.show()
+            sleep(0.02)
+
+
+def brightness(pix: pixels.Pixels) -> None:
+    delta = 1 / len(pix)
+    while True:
+        pix.fill(pixels.Color(255, 0, 0))
+        pix.show()
+        input()
+        # pix[0] = pixels.Color(100, 0, 0)
+        for i in range(len(pix)):
+            pix[i]._b = 255
+        for c in pix:
+            sys.stderr.write(f"{c.get_rgb()}\n")
+        pix.show()
+        input()
+        continue
+        for i, c in enumerate(pix):
+            pix[i].brightness = (i + 1) * delta
+            pix.show()
+            sleep(0.04)
 
 
 def main() -> None:
-    pix = pixels.Pixels(N)
-    delta = 255 // len(pix)
-    toggle = True
-    offset = {True: 0, False: len(pix) - 1}
-    while True:
-        for i in range(len(pix)):
-            pix[i - offset[toggle]] = pixels.Color(
-                i * delta, 255 - i * delta, 50
-            )
-            pix.show()
-            time.sleep(DELAY)
-        pix.off()
-        toggle = not toggle
+    with pixels.Pixels(500) as pix:
+        try:
+            brightness(pix)
+        except KeyboardInterrupt:
+            print()
 
 
 if __name__ == "__main__":
--- pixels.py
+++ pixels.py
@@ -1,51 +1,140 @@
+import shutil
+import sys
+from math import ceil
+from typing import TextIO
+
+# pyright: strict
+
+WIDTH, HEIGHT = shutil.get_terminal_size()
+
+
 class Color:
-    def __init__(self, red: int, blue: int, green: int) -> None:
-        self.r = red
-        self.g = green
-        self.b = blue
+    def __init__(self, r: int, g: int, b: int) -> None:
+        self._r = r
+        self._g = g
+        self._b = b
+        self.brightness: float = 1
+
+    def get_rgb(self) -> tuple[int, int, int]:
+        assert (
+            self.brightness >= 0
+        ), "brightnesss must be greater than or equal to zero"
+        return (
+            round(self._r * self.brightness),
+            round(self._g * self.brightness),
+            round(self._b * self.brightness),
+        )
 
     def __repr__(self) -> str:
-        return f"Color(({self.r}, {self.g}, {self.b}))"
+        return f"Color(({self._r}, {self._g}, {self._b}))"
 
     def __str__(self) -> str:
-        return str((self.r, self.g, self.b))
+        return str((self._r, self._g, self._b))
+
+
+class Screen:
+    def __init__(self, file: TextIO = sys.stdout) -> None:
+        self._file = file
+
+    def clear(self):
+        self._file.write("\x1b[2J")
+
+    def hide_cursor(self) -> None:
+        self._file.write("\x1b[?25l")
+
+    def show_cursor(self) -> None:
+        self._file.write("\x1b[?25h")
+
+    def set_cursor(self, x: int = 1, y: int = 1) -> None:
+        self._file.write(f"\x1b[{y};{x}H")
+
+    def print_color(self, msg: str, r: int, g: int, b: int) -> None:
+        self._file.write(f"\x1b[38;2;{r};{g};{b}m{msg}")
+
+    def flush(self) -> None:
+        self._file.flush()
+
+    def print(self, msg: str) -> int:
+        return self._file.write(msg)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self) -> None:
+        self.flush()
+        self.show_cursor()
+        self._file.close()
 
 
 class Pixels:
     def __init__(
-        self, n: int, fill: Color = Color(0, 0, 0), auto_show: bool = False
+        self,
+        n: int,
+        pixel_str: str = "■ ",
+        header: str = "[ ",
+        footer: str = "]",
+        screen: Screen | None = None,
     ) -> None:
-        self._array: list[Color] = [fill] * n
-        self.auto_show = auto_show
+        self.pix_str = pixel_str
+        self.header = header
+        self.footer = footer
+        self._pixel_array = [Color(0, 0, 0)] * n
+        self._changed = {i: True for i in range(len(self._pixel_array))}
+
+        if screen is None:
+            screen = Screen().__enter__()
+        self._screen = screen
+        self._screen.hide_cursor()
+        self._screen.clear()
+        self._screen.set_cursor()
+        self._screen.print(self.header)
+        self._screen.set_cursor(
+            (len(self) + len(self.pix_str)) * len(self.pix_str) % WIDTH,
+            ceil(len(self) * len(self.pix_str) / WIDTH),
+        )
+        self._screen.print(self.footer)
 
     def fill(self, color: Color) -> None:
-        self._array = [color] * len(self)
-        if self.auto_show:
-            self.show()
-
-    def off(self) -> None:
-        self._array = [Color(0, 0, 0)] * len(self)
-        self.show()
+        self._pixel_array = [color] * len(self)
+        self._changed = {i: True for i in range(len(self._pixel_array))}
 
     def show(self) -> None:
-        print("\x1b[2J\x1b[1;1H", end="")
-        for c in self._array:
-            print(f"\x1b[38;2;{c.r};{c.g};{c.b}m■ ", end="")
-        print()
+        x = len(self.header) + 1
+        for i, c in enumerate(self._pixel_array):
+            y = ceil(x / WIDTH)
+            if self._changed[i]:
+                self._screen.set_cursor(x % WIDTH, y)
+                self._screen.print_color(self.pix_str, *c.get_rgb())
+                self._changed[i] = False
+            x += len(self.pix_str)
+        self._screen.flush()
 
     def __getitem__(self, i: int) -> Color:
-        return self._array[i]
+        return self._pixel_array[i]
 
     def __setitem__(self, i: int, c: Color) -> None:
-        self._array[i] = c
-        if self.auto_show:
-            self.show()
+        self._pixel_array[i] = c
+        self._changed[i] = True
+
+    # def __iter__(self):
+    #    i = 0
+    #    while i < len(self):
+    #        yield self[i]
+    #        i += 1
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_tb) -> None:  # pyright: ignore
+        self._screen.__exit__()
 
     def __len__(self) -> int:
-        return len(self._array)
+        return len(self._pixel_array)
 
     def __repr__(self) -> str:
-        return f"Pixels({len(self)})"
+        return (
+            f"Pixels({len(self)}, {self.pix_str} ,{self.header}, {self.footer})"
+        )
 
     def __str__(self) -> str:
-        return str(self._array)
+        return str(self._pixel_array)
