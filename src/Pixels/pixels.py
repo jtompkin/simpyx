385ef487fa76b9ba4792f47652926678b60f39e2
jtompkin <jtompkin-dev@pm.me>
2024-08-29 11:53:29 -0400

    packaging

--- src/Pixels/pixels.py
+++ src/Pixels/pixels.py
@@ -1,45 +1,13 @@
 import shutil
-import sys
+from collections.abc import Callable
 from math import ceil
-from typing import TextIO
+from typing import Self
+
+from screen import Screen
 
 # pyright: strict
 
 WIDTH, HEIGHT = shutil.get_terminal_size()
-
-
-class Screen:
-    def __init__(self, file: TextIO = sys.stdout) -> None:
-        self._file = file
-
-    def clear(self):
-        self._file.write("\x1b[2J")
-
-    def hide_cursor(self) -> None:
-        self._file.write("\x1b[?25l")
-
-    def show_cursor(self) -> None:
-        self._file.write("\x1b[?25h")
-
-    def set_cursor(self, x: int = 1, y: int = 1) -> None:
-        self._file.write(f"\x1b[{y};{x}H")
-
-    def print_color(self, msg: str, r: int, g: int, b: int) -> None:
-        self._file.write(f"\x1b[38;2;{r};{g};{b}m{msg}")
-
-    def print(self, msg: str) -> int:
-        return self._file.write(msg)
-
-    def flush(self) -> None:
-        self._file.flush()
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self) -> None:
-        self.flush()
-        self.show_cursor()
-        self._file.close()
 
 
 class Pixel:
@@ -132,6 +100,9 @@ class PixelDrawer:
                 p.changed = False
             x += len(self.pix_str)
         self._screen.flush()
+
+    def run(self, program: Callable[[Self], None]) -> None:
+        program(self)
 
     def __getitem__(self, i: int) -> Pixel:
         return self._pixel_array[i]
---
+++ src/Shows/shows.py
@@ -0,0 +1,33 @@
+from time import sleep
+
+from Pixels import pixels
+
+DELAY = 0.05
+
+
+def static(pix: pixels.PixelDrawer) -> None:
+    pix[3] = pixels.Pixel(100, 20, 255)
+    pix.show()
+    input()
+
+
+def cycle(pix: pixels.PixelDrawer) -> None:
+    delta = 255 / len(pix)
+    while True:
+        for i, p in enumerate(pix):
+            p.set_rgb(round((i + 1) * delta), 0, 100)
+            pix.show()
+            sleep(DELAY)
+        pix.fill(0, 0, 0)
+        pix.show()
+
+
+def brightness(pix: pixels.PixelDrawer) -> None:
+    delta = 1 / len(pix)
+    while True:
+        pix.fill(255, 0, 0)
+        pix.show()
+        for i, p in enumerate(pix):
+            p.brightness = (i + 1) * delta
+            pix.show()
+            sleep(DELAY)
